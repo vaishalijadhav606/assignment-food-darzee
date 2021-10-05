6671ba69325598412bc2d75a34a59e98f3fa2829
vaishali-jadhav <vaishali.jadhav@colortokens.com>
2021-10-06 00:31:37 +0530

    random color added

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const CardContent = styled.div`
   margin: 10px 10px 0 0;
   border-radius: 2px;
-  border: 3px solid #000;
+  border: 2px solid #000;
   width: 70px;
   height: 70px;
   display: flex;
@@ -12,9 +12,20 @@ const CardContent = styled.div`
   justify-content: center;
 `;
 
+const color = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']
+
 export const Card = (props) => {
   return (
-      <CardContent>
+      <CardContent style={{backgroundColor: `${color[Math.floor(Math.random() * (color.length - 1))]}`}}>
           <div>Text</div>
       </CardContent>
   )
